322be1af075e47111fee879da19cbf4c593d5e72
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-07-05 22:02:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 function nextPalindrome(num) {
   //your JS code here.
+	function isPalindrome(num) {
+        let originalNum = num;
+        let reversedNum = 0;
+        
+        while (num > 0) {
+            reversedNum = reversedNum * 10 + num % 10;
+            num = (num - num % 10) / 10; // Equivalent to integer division without Math.floor
+        }
+        
+        return originalNum === reversedNum;
+    }
+
+    // Convert input to number
+    num = parseInt(num);
+
+    // Start checking from the next number
+    let nextNumber = num + 1;
+    
+    // Loop until we find a palindrome
+    while (!isPalindrome(nextNumber)) {
+        nextNumber++;
+    }
+
+    return nextNumber;
 }
 
 const input = prompt("Enter a palindrome number");
